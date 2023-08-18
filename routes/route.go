ac12c7eb7a67f66905409f0ff97fa789ae3d3cc6
Nurizalludin <nurizalludinr@gmail.com>
2023-08-18 18:41:23 +0700

    add welcome page

--- routes/route.go
+++ routes/route.go
@@ -11,8 +11,8 @@ func InitRoute(r *gin.Engine) {
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, "Welcome to API")
 	})
-	r.POST("/register", controllers.Register)
-	r.POST("/login", controllers.Login)
+	r.POST("/api/register", controllers.Register)
+	r.POST("api//login", controllers.Login)
 	r.GET("/api/meds", controllers.GetMeds)
 
 	auth := r.Group("")
