7fc3445e0a1455981eaa1bec567baa5eca44747f
okis <okiiskandar85@gmail.com>
2023-08-19 14:04:30 +0700

    login

--- models/user.go
+++ models/user.go
@@ -6,6 +6,7 @@ import (
 
 type User struct {
 	gorm.Model
+	ID       uint   `json:"id" gorm:"primaryKey"`
 	Username string `gorm:"size:255;not null;unique" json:"username"`
 	Password string `gorm:"size:255;not null;" json:"password"`
 }
